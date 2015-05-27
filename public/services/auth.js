af4d35484607ab84f77475e7f07cca0e4300885e
Inho Yong <inho.yong@vocitec.com>
2015-05-26 22:57:21 -0400

    remove unused ajax

--- public/services/auth.js
+++ public/services/auth.js
@@ -29,57 +29,12 @@ angular.module('Hiemino')
               });
             });
         },
-        test: function(user) {
-          return $http.get('/api/createaccount')
-            .success(function(datat) {
-              $rootScope.currentUser = data;
-              $location.path('/home');
-
-              $alert({
-                title: 'Cheers!',
-                content: 'You have successfully created account.',
-                placement: 'top-right',
-                type: 'success',
-                duration: 10
-              });
-            })
-            .error(function() {
-              $alert({
-                title: 'Error!',
-                content: 'Create account failed',
-                placement: 'top-right',
-                type: 'danger',
-                duration: 10
-              });
-            });
-        },
-        // signup: function(user) {
-        //   return $http.post('/api/signup', user)
-        //     .success(function() {
-        //       $location.path('/login');
-
-        //       $alert({
-        //         title: 'Congratulations!',
-        //         content: 'Your account has been created.',
-        //         placement: 'top-right',
-        //         type: 'success',
-        //         duration: 10
-        //       });
-        //     })
-        //     .error(function(response) {
-        //       $alert({
-        //         title: 'Error!',
-        //         content: response.data,
-        //         placement: 'top-right',
-        //         type: 'danger',
-        //         duration: 10
-        //       });
-        //     });
-        // },
         logout: function() {
           return $http.get('/api/logout').success(function() {
             $rootScope.currentUser = null;
             $cookieStore.remove('user');
+            $location.path('/');
+            
             $alert({
               content: 'You have been logged out.',
               placement: 'top-right',
